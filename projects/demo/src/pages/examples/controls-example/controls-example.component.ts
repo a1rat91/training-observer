f2754adbe5e68d958e907572d842b07726334b63
Airat Khairullin <a1rat91@yandex.ru>
2026-09-11 20:17:48 +0300

    feat: poc init

--- projects/demo/src/pages/examples/controls-example/controls-example.component.ts
+++ projects/demo/src/pages/examples/controls-example/controls-example.component.ts
@@ -1,19 +1,16 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TuiAddonDoc} from '@taiga-ui/addon-doc';
 import {TuiButton} from '@taiga-ui/core';
 
 import {DemoMicrofrontendComponent} from '../../../components/demo-microfrontend/demo-microfrontend.component';
-import {ObserverPanelComponent} from '../../../components/observer-panel';
 
 @Component({
     standalone: true,
     selector: 'controls-example',
-    imports: [DemoMicrofrontendComponent, ObserverPanelComponent, TuiAddonDoc, TuiButton],
+    imports: [DemoMicrofrontendComponent, TuiAddonDoc, TuiButton],
     templateUrl: './controls-example.component.html',
     styleUrl: './controls-example.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [],
 })
-export default class ControlsExampleComponent {
-    public readonly showInspector = signal(true);
-}
+export default class ControlsExampleComponent {}
